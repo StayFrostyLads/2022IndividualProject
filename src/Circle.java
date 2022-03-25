53026e2f81fa3ca5b33b768b7f9e1950331da33e
StayFrostyLads <59467621+StayFrostyLads@users.noreply.github.com>
2022-03-24 23:42:11 -0600

    Update Circle.java

--- src/Circle.java
+++ src/Circle.java
@@ -7,19 +7,19 @@ import java.awt.event.*;
 
 public class Circle extends JFrame {
 	
-	static double time;
-	int size, x, y;
+	static double time, x, y;
+	int radius;
 
 	/*
 	 * Constructor
 	 */
 	public Circle() {
-		setTitle("Drawing a Circle");
-        setSize(300, 300);
+		setTitle("Drawing Fourier Series");
+        setSize(1000, 1000);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         time = 0;
-        size = 0;
+        radius = 0;
 
 	}
 	
@@ -31,29 +31,40 @@ public class Circle extends JFrame {
 	@Override
     public void paint(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-        
-        Ellipse2D circle1 = new Ellipse2D.Float(x, y, size, size);
-        circle1.setFrame(x, y, size, size);
-        g2d.draw (circle1);
-        g2d.drawLine(0,0,0,0);
+        while (time < 360) {
+        	radius = 100;
+            g2d.translate(0, 0);
+            Ellipse2D circle1 = new Ellipse2D.Float(0, 0, radius*2, radius*2);
+            circle1.setFrame(20, 120, radius, radius);
+            g2d.draw (circle1);
+            
+            x = radius * Math.cos(time);
+            y = radius * Math.sin(time);
+            g2d.translate(20,120);
+            Ellipse2D.Double circle2 = new Ellipse2D.Double(x, y, 3, 3);
+            g2d.fill (circle2);
+            for (int i = 0; i < 360; i+=360) {
+            g2d.rotate (Math.toRadians(i), x + radius * 2, y + radius * 2);
+            time += 0.01;
+            g2d.draw (circle2);
+            }
+        }
     }
 	
 	@Override
 	public void setLocation(int x, int y) {
-		super.setLocation(x-(size/2), y-(size/2));
+		super.setLocation(x-(radius/2), y-(radius/2));
 	}
 	@Override
 	public void setLocation(Point p) {
-		super.setLocation((int)p.getX()-size/2,(int)p.getY()-size/2);
+		super.setLocation((int)p.getX()-radius/2,(int)p.getY()-radius/2);
 	}
     public static void main(String[] args) {
 
        Circle firstCircle = new Circle();
        firstCircle.setVisible(true);
        
-//       while () {
-//    	   time += 0.01;
-//       }
+
        
 
     }
