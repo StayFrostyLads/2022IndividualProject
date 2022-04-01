32a5d7046cdf68e390a17b0000ae35182836cff3
StayFrostyLads <59467621+StayFrostyLads@users.noreply.github.com>
2022-04-01 15:15:15 -0600

    Update InformationPanel.java

--- src/InformationPanel.java
+++ src/InformationPanel.java
@@ -13,13 +13,17 @@ import java.awt.*;
  */
 public class InformationPanel extends JPanel {
 	public static JButton home, quit;
-	private JLabel title;
+	private JLabel title, info;
 	
 	public InformationPanel() {
 		setLayout(null);
 		title = new JLabel("Information"); 
 		title.setBounds(325,20,200,100);
 		add(title);
+		
+		info = new JLabel("<html>A Fourier series is a periodic function in terms of an infinite sum of sines and cosines.<br/>A Fourier series is able to draw the images contained in the application through something known as a Fourier transform.<br/>A Fourier transform method uses a set of x and y coordinates to calculate the epicycles (circle moving around another circle) needed to draw the image.<br/>The Epicycles then rotate in a certain order at a certain pace to create the images you see in the application.</html>");
+		info.setBounds(200, 200, 350, 350);
+		add(info);
 		
 		home = new JButton("Back to Home");
 		home.setBounds(250,600,300,50);
