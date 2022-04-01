0910bb1bbbb32488127274592dfbf04eac038631
StayFrostyLads <59467621+StayFrostyLads@users.noreply.github.com>
2022-04-01 15:14:51 -0600

    Update TitlePanel.java

--- src/TitlePanel.java
+++ src/TitlePanel.java
@@ -1,10 +1,19 @@
+/*
+ * The TitlePanel class functions as the primary page
+ * that users view when they run the application.
+ * 
+ * 
+ * @author Jack Nebeker
+ * @version 1.0.0
+ * @since 2022-03-23
+ */
+
 package panel;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-// Inheritance Y2 concept
 public class TitlePanel extends JPanel {
 	public static JButton pi, zelda, amongus, naruto, information, quit;
 	// Declaring the buttons that the user will press to access other pages
@@ -14,11 +23,6 @@ public class TitlePanel extends JPanel {
 	public TitlePanel() {
 		setLayout(null);
 		title = new JLabel("Drawing Fourier Series For You!"); 
-		
-		title.setBounds(325,20,200,100);
-	
-		add(title);
-		
 		pi = new JButton("Pi"); 
 		zelda = new JButton("Zelda Triforce Symbol"); 
 		amongus = new JButton("Among Us Character");
@@ -26,6 +30,7 @@ public class TitlePanel extends JPanel {
 		information = new JButton("Basic Information on Fourier Series");
 		quit = new JButton("Exit Application");
 		
+		title.setBounds(325,20,200,100);
 		pi.setBounds(30,300,100,50);
 		zelda.setBounds(160,300,200,50);
 		amongus.setBounds(390,300,200,50);
@@ -33,6 +38,7 @@ public class TitlePanel extends JPanel {
 		information.setBounds(250,600,300,50);
 		quit.setBounds(620,20,150,50);
 		
+		add(title);
 		add(pi);
 		add(zelda);
 		add(amongus);
